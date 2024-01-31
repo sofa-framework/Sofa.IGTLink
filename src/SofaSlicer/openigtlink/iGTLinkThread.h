e37d0e4a052d0fa72a985a17f29b22a0d3292c24
bakpaul <paul.baksic@outlook.fr>
2024-01-31 19:25:15 +0100

    Thread compiling but not tested

--- src/SofaSlicer/openigtlink/iGTLinkThread.h
+++ src/SofaSlicer/openigtlink/iGTLinkThread.h
@@ -1,54 +1,53 @@
-//#pragma once
-//
-//#include <thread>
-//#include <functional>
-//#include <memory>
-//#include <atomic>
-//#include <mutex>
-//#include <SofaSlicer/openigtlink/iGTLinkMessage.h>
-//#include <sofa/core/objectmodel/Data.h>
-//
-//class iGTLinkBase;
-//using namespace sofa::core::objectmodel;
-//
-//namespace SofaSlicer::openigtlink
-//{
-//    class iGTLinkBaseThread
-//    {
-//    public:
-//        iGTLinkBaseThread(iGTLinkBase* _link) : m_link(_link), m_thread(nullptr){}
-//
-//        void mainLoop();
-//
-//        virtual void doLoop() = 0;
-//
-//        void launchThread();
-//        void stopThread();
-//
-//    protected:
-//        std::atomic<bool> m_running;
-//        std::shared_ptr<std::thread> m_thread;
-//        iGTLinkBase * m_link;
-//    };
-//
-//    class iGTLinkRecieverThread : public iGTLinkBaseThread
-//    {
-//    public:
-//        iGTLinkRecieverThread();
-//
-//        virtual void doLoop();
-//
-//        std::map<std::string,igtl::MessageBase::Pointer>& getAvailableData();
-//
-//    private:
-//        void swapData();
-//
-//        std::atomic<unsigned> m_currentData;
-//        std::map<std::string,igtl::MessageBase::Pointer> m_dataStructures[2];
-//        std::mutex m_mutex[2];
-//        Data<int> d_timeout;
-//
-//
-//    };
-//
-//}
+#pragma once
+
+#include <thread>
+#include <functional>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <SofaSlicer/openigtlink/iGTLinkMessage.h>
+#include <sofa/core/objectmodel/Data.h>
+
+class iGTLinkBase;
+using namespace sofa::core::objectmodel;
+
+namespace SofaSlicer::openigtlink
+{
+    class iGTLinkBaseThread
+    {
+    public:
+        iGTLinkBaseThread(iGTLinkBase* _link) : m_link(_link), m_thread(nullptr){}
+
+        void mainLoop();
+
+        virtual void doLoop() = 0;
+
+        void launchThread();
+        void stopThread();
+
+    protected:
+        std::atomic<bool> m_running;
+        std::shared_ptr<std::thread> m_thread;
+        iGTLinkBase * m_link;
+    };
+
+    class iGTLinkRecieverThread : public iGTLinkBaseThread
+    {
+    public:
+        iGTLinkRecieverThread(iGTLinkBase* _link);
+
+        virtual void doLoop();
+
+        std::map<std::string,igtl::MessageBase::Pointer>& getAvailableData();
+
+    private:
+        void swapData();
+
+        std::atomic<unsigned> m_currentData;
+        std::map<std::string,igtl::MessageBase::Pointer> m_dataStructures[2];
+        std::mutex m_mutex[2];
+
+
+    };
+
+}
